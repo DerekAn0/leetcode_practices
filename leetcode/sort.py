4b9220b15ba46ade6eda6e05d93314b811dfc078
PC-YANG <hegongdaanyang@outlook.com>
2019-10-02 22:54:44 +0200

    [FIX] rethinking on sort structure and adding insert sort and bubble sort

--- leetcode/sort.py
+++ leetcode/sort.py
@@ -1,11 +1,34 @@
 class Sort:
-	def __init__(self):
-		self.test_list =[ 2,3,4,5,2]
+	test_list = [49,38,65,97,76,13,27,49]
+	after_list = test_list.copy()
+
+	@classmethod
+	def __str__(cls):
+		return '{0.after_list}'.format(cls)
 
 	def Quick(self):
-		print(self.test_list)
+		pass
 
-a = [1,3,4]
-def use_lambda(num:list)->int:
-	return lambda num:max(num)
-print(use_lambda.__annotations__)
+	@classmethod
+	def erase_after_list(cls):
+		cls.after_list = cls.test_list
+
+
+	@classmethod
+	def Direct_Insert(cls):
+		pass
+
+
+	@classmethod
+	def Bubble(cls):
+		print(cls.test_list)
+		n = len(cls.after_list)
+		for i in range(n-1):
+			for j in range(n-i-1):
+				if cls.after_list[j] > cls.after_list[j+1]:
+					cls.after_list[j],cls.after_list[j+1] = cls.after_list[j+1],cls.after_list[j]
+
+
+
+Sort.Bubble()
+print(Sort())
