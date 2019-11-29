d027ead53e0d7fea718f9d910abc1465c0334039
Yang AN <hegongdaanyang@outlook.com>
2019-11-29 17:11:53 +0100

    [on process]

--- leetcode/Array/1266_Minimum_time_Visiting_All_Points.py
+++ leetcode/Array/1266_Minimum_time_Visiting_All_Points.py
@@ -1,24 +1,23 @@
+import math
 class Solution:
     def minTimeToVisitAllPoints(self, points):
         sum = 0
         for i in range(0, len(points)-1):
             j = i+1
-            sum += self.caculate_distance(points[i], points[j])
+            sum += (self.caculate_distance(points[i], points[j]))
         return sum
 
-
-
     def caculate_distance(self, point1, point2):
-        num1 = point1[0] - point1[0]
-        num2 = point1[1] -point1[1]
+        num1 = math.fabs(point1[0] - point2[0])
+        num2 = math.fabs(point1[1] - point2[1])
         if num1 == num2:
-        	return num1
+        	return math.fabs(num1)
         elif num1 > num2:
-        	return num1
+        	return math.fabs(num1)
         else:
-        	return num2
+            return math.fabs(num2)
 
 
 sol = Solution()
-sum = sol.minTimeToVisitAllPoints([[1, 1], [3, 4], [-1, 0]])
+sum = sol.minTimeToVisitAllPoints([[3,2],[-2,2]])
 print(sum)
